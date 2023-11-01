3db7ebab3eeb8b08cccaf848a320fb7a0770fa82
Kyle-207 <send.zhikai@gmail.com>
2023-11-01 15:00:28 -0700

    Added olympic judging

---
+++ olympic_judging.py
@@ -0,0 +1,14 @@
+# Olympic Judging
+# Author: Kyle Wang
+# 1 November 2023
+
+# Greet the user
+
+
+
+# Get 5 scores for the user
+# Each is out of 10 points maximum
+
+# Calculate the average score
+
+# Print the average score to the user
