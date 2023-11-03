9ca9a383fc9d39585302cc68a566bc7531eccc40
Kyle-207 <send.zhikai@gmail.com>
2023-11-03 09:50:31 -0700

    Finished WTB

--- world_traveller_bot.py
+++ world_traveller_bot.py
@@ -1,3 +1,23 @@
 # World Traveller Bot
 # Author: Kyle Wang
 # Nov 3 2023
+
+# Greet the user and ask for their name
+
+user_name = input("What is your name?").strip(",.?!")
+
+print(f"Hello, {user_name}! Nice to meet you. I am the World Traveller Bot that counts how many continents you've been to.")
+
+# Ask if the user has been to each of the continents
+
+continents = ["Asia", "Europe", "North America", "South America", "Australia", "Africa", "Antarctica"]
+con_counts = 0
+
+for continent in continents:
+    been_to_c = input("Have you been to" + continent + "? (Yes / No)")
+    if been_to_c.lower.strip(",.?! ") == "yes":
+        con_counts += 1
+
+# When finished asking, tell the user how many continents they've been to. 
+
+print(f"I see, you've visited {con_counts} / 7 continents.")
