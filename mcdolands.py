77fd049d6b13c9d1ead68b49ad45f9d887d6a5db
Kyle-207 <send.zhikai@gmail.com>
2023-11-03 09:33:08 -0700

    Mcdolands and Olympic Judging

---
+++ mcdolands.py
@@ -0,0 +1,34 @@
+# McDoland's
+# Author: Kyle Wang
+# 3 Nov 2023
+
+import time
+
+# Greet the user
+
+print("Hi, this is the McDoland's ordering program, you can order some food here.")
+
+# Ask if the user would like a burger and/or fries
+
+need_burger = input("Would you like a burger for $5? (Yes / No)")
+need_fries = input("Would you like fries for $3? (Yes / No)")
+
+time.sleep(1)
+
+order_cost = 0
+
+if need_burger.lower().strip(",.?! ") == "yes":
+    order_cost += 5
+
+if need_fries.lower().strip(",.?! ") == "yes":
+    order_cost += 3
+
+# Calculate the total money with a 14% tax
+
+tax_rate = 0.14         # 14%
+
+final_cost = round(order_cost * (1 + tax_rate), 2)
+
+# Print out the total to the user
+
+print(f"Your total is ${final_cost}")
--- olympic_judging.py
+++ olympic_judging.py
@@ -2,13 +2,34 @@
 # Author: Kyle Wang
 # 1 November 2023
 
+import time
+
 # Greet the user
 
-
+print("Hello, this is an Olympic Judging program. You'll need to input 5 scores, and I'll give you the average.")
 
 # Get 5 scores for the user
+judges = ["Judge 1", "Judge 2", "Judge 3", "Judge 4", "Judge 5"]
+
 # Each is out of 10 points maximum
+time.sleep(1)
+print("Each score is out 10 points maximum, you can use half points if you want.")
+
+final_score = 0
+
+for judge in judges:
+    score = float(input(judge + ": ").strip(",.?! "))
+    if score >= 0 and score <= 10:
+        final_score += score
+    else:
+        print("Your input score does not match the restrictions, please type again.")
+        score = float(input(judge + ": ").strip(",.?! "))
 
 # Calculate the average score
 
+average_score = round(final_score / len(judges), 2)
+
 # Print the average score to the user
+
+time.sleep(1)
+print(f"Your Olympic score is: {average_score} / 10")
