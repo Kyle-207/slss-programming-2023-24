44f577ef71c0a9b7c9fe12a5a098fa6de64d126e
Kyle-207 <send.zhikai@gmail.com>
2023-11-07 13:55:06 -0800

    Added on age in 2049 file

--- age_in_2049.py
+++ age_in_2049.py
@@ -17,5 +17,9 @@ x = datetime.datetime.now()
 future_age = user_age + (2049 - x.year)
 
 # Show the result to the user
-
-print(f"You will be {future_age} years old in 2049.")
+if x.year < 2049:
+    print(f"You will be {future_age} years old in 2049.")
+elif x.year == 2049:
+    print(f"It is 2049 right now! So you are either {user_age} years old or you'll be {user_age + 1} years old after your birthday!")
+elif x.year > 2049:
+    print(f"2049 is over, so you were {future_age} years old before in 2049.")
