c678a3bc27c75760f30759408c5f1cd1724c1750
Kyle-207 <send.zhikai@gmail.com>
2023-11-14 09:50:48 -0800

    Parental bot

---
+++ parental_bot.py
@@ -0,0 +1,25 @@
+# Parental Bot
+# Author: Kyle Wang
+# Nov 14, 2023
+
+# Create 4 questions for the parental bot
+
+questions = [
+    "Did you eat?",
+    "Did you study?",
+    "Did you do your laundry?",
+    "Did you call grandma?"
+    ]
+
+# When the user answers "yes", user's points will increase by 1
+
+points = 0
+
+for question in questions:
+    answer = input().lower().strip(",.?/! ")
+
+    if answer == "yes":
+        points += 1
+
+# At the end, reply to the user depending on the points the user has
+
