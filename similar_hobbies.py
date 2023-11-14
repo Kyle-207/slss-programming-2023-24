8b0c76021d7b2b427384b752a9b479669dd9f8b5
Kyle-207 <send.zhikai@gmail.com>
2023-11-14 09:42:23 -0800

    Similar hobbies

---
+++ similar_hobbies.py
@@ -0,0 +1,28 @@
+# Similar Hobbies
+# Author: Kyle Wang
+# Nov 14, 2023
+
+# Get the hobbies from two different users
+
+user_1_h = input("User 1, please enter your hobbies, separated by spaces. Please use underscores to replace with spaces.")
+user_2_h = input("User 2, please enter your hobbies, separated by spaces. Please use underscores to replace with spacese.")
+
+print(f"User 1: {user_1_h}")
+print(f"User 2: {user_2_h}")
+
+# Create a similarity score for the hobbies they have in common
+
+sim_score = 0
+
+# Compare the hobbies and count the sim score
+
+for hobby in user_1_h.lower().strip(",./?!").split(" "):
+    if hobby in user_2_h.lower().strip(",./?!").split(" "):
+        sim_score += 1
+
+# Print out the sim score
+
+if sim_score == 1:
+    print(f"You two have {sim_score} hobby in common!")
+else:
+    print(f"You two have {sim_score} hobbies in common!")
