090eeefb089afec191dc3510897a109af0ba7742
Kyle-207 <send.zhikai@gmail.com>
2023-11-22 11:10:35 -0800

    Added cookies

---
+++ cookies.py
@@ -0,0 +1,59 @@
+# Cookies
+# Author: Kyle Wang
+# 21 November 2023
+
+import random
+import turtle
+
+
+
+
+# Make baker turtle
+baker_turtle = turtle.Turtle()
+baker_turtle.color("brown")
+
+# Design a function that creates a cookie at x and y
+def make_cookie(x: int, y: int):
+    """Creates a cookice on the screen
+    Draws it at location (x, y)
+    
+    Params:
+    x - the x-location of the centre
+    y - the y-location of the centre"""
+    baker_turtle.color("brown")
+    baker_turtle.penup()
+    baker_turtle.goto(-5 + x, -30 + y)
+    baker_turtle.pendown()
+    baker_turtle.circle(30)
+
+make_cookie(100, 30)
+
+# Draw outline of cookie
+baker_turtle.penup()
+baker_turtle.goto(-5, -30)
+baker_turtle.pendown()
+baker_turtle.circle(30)
+baker_turtle.penup()
+
+# Add chips
+
+baker_turtle.color("black")
+baker_turtle.goto(0,0)
+baker_turtle.stamp()
+
+# Add top right, bottom right, top left, bottom left
+baker_turtle.goto(10, 10)
+baker_turtle.stamp()
+baker_turtle.goto(10, -10)
+baker_turtle.stamp()
+baker_turtle.goto(-10, 10)
+baker_turtle.stamp()
+baker_turtle.goto(-10, -10)
+baker_turtle.stamp()
+
+
+
+
+
+turtle.done()
+
