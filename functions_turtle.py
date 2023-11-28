4e75a842d9e977bf966b34622b43b5816d5979f5
Kyle-207 <send.zhikai@gmail.com>
2023-11-28 09:33:52 -0800

    Functions turtle

--- functions_turtle.py
+++ functions_turtle.py
@@ -1,3 +1,37 @@
 # Functions Turtle
 # Author: Kyle Wang
 # 28 November 2023
+
+import turtle
+
+burt = turtle.Turtle()
+
+burt.color("lightblue")
+
+def squared(x: float) -> float:
+    """Takes a num and squares it and returns it"""
+
+    return x ** 2
+
+def draw_square(x: int) -> None:
+    burt.fd(x)
+    burt.lt(90)
+    burt.fd(x)
+    burt.lt(90)
+    burt.fd(x)
+    burt.lt(90)
+    burt.fd(x)
+    burt.lt(90)
+
+draw_square(50)
+
+burt.speed(0)
+burt.penup()
+burt.goto(-200,-200)
+burt.pendown()
+# Draw squares that grow exponentially
+for i in range(25):
+    draw_square(squared(i))
+
+
+turtle.done()
