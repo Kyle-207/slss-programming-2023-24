056f7083edaf014ab8ea1b455f5b3014e2743aa1
Kyle-207 <send.zhikai@gmail.com>
2023-12-18 14:05:02 -0800

    Is light function

--- colour_helper.py
+++ colour_helper.py
@@ -22,3 +22,32 @@ def pixel_to_string(pixel: tuple) -> str:
         return "green"
     elif (g > 172 and r < 75 and b < 91) or (g > 134 and r < 101 and b < 32) or (g >= 190 and r <= 84 and b <= 107):
         return "green"
+    
+def is_light(pixel: tuple) -> bool:
+    """Take a rgb 3-tuple. If the average 
+    is greater than or equal to 128, 
+    return True, if not, return False.
+
+    Params: 
+        pixel - 3-tuple of (red, green, blue)
+        
+    Return:
+        Boolean value according to the average
+    """
+
+    r, g, b = pixel
+
+    if ((r + g + b) / 3) >= 128:
+        return True
+    else:
+        return False
+    
+black_pixel = (0, 0, 0)
+dark_gray_pixel = (127, 127, 127)
+light_gray_pixel = (128, 128, 128)
+white_pixel = (255, 255, 255)
+
+print(is_light(black_pixel))      # False
+print(is_light(dark_gray_pixel))  # False
+print(is_light(light_gray_pixel)) # True
+print(is_light(white_pixel))      # True
